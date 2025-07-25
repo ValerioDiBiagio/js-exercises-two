727aef73421a64150ce7e5c8ae4c265a3f1d85b6
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-25 17:01:31 +0200

    add second ex

--- exercise.js
+++ exercise.js
@@ -1,16 +1,30 @@
 // Scrivi una funzione chiamata contaVocali che prende una stringa e restituisce il numero di vocali presenti (a, e, i, o, u, anche maiuscole).
 
-function contaVocali(word) {
-    let count = 0;
-    const vocali = "aeiouAEIOU"
+{
 
-    for (let i = 0; i < word.length; i++) {
+    function contaVocali(word) {
+        let count = 0;
+        const vocali = "aeiouAEIOU"
 
-        if (vocali.includes(word[i])) {
-            count++;
+        for (let i = 0; i < word.length; i++) {
+
+            if (vocali.includes(word[i])) {
+                count++;
+            }
         }
+        return count;
     }
-    return count;
+
+    console.log(contaVocali("Ciao"));
+
 }
 
-console.log(contaVocali("Ciao"));
+// Dato l’array:
+// const colori = ["rosso", "verde", "blu", "giallo"];
+// Stampa il numero totale di elementi contenuti nell’array.
+
+{
+    const colori = ["rosso", "verde", "blu", "giallo"];
+    console.log(colori.length);
+}
+

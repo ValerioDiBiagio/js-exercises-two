fbff6e9b4152e97b2c0429ceadef88436339f124
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-07-29 11:01:49 +0200

    add ninth ex

--- exercise.js
+++ exercise.js
@@ -131,3 +131,17 @@
 
 
 }
+
+// Somma dei numeri da 1 a 100
+// Scrivi un ciclo for che calcoli e stampi la somma di tutti i numeri da 1 a 100.
+
+{
+    let som = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        som += i
+    }
+
+    console.log(som)
+}
+
